7650b9420d40c68ab899ba67e2d41212833ba081
Martin Beauvais <mbeauv@gmail.com>
2018-03-02 21:10:17 -0500

    Minor change to test app initializer

--- spec/dummy/config/initializers/media_gallery_initializer.rb
+++ spec/dummy/config/initializers/media_gallery_initializer.rb
@@ -1,9 +1,5 @@
 MediaGallery::ApplicationController.class_eval do
 
-  def current_user
-    User.find_by_token(request.headers['token'])
-  end
-
   def create_ability(user)
     Ability.new(user)
   end
