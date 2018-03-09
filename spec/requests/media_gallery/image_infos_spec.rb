6478f77715dc1946606abc963bdd779f5480ebdc
Martin Beauvais <mbeauv@gmail.com>
2018-03-08 21:18:44 -0500

    Restructure unit tests

--- spec/requests/media_gallery/image_infos_spec.rb
+++ spec/requests/media_gallery/image_infos_spec.rb
@@ -1,99 +1,146 @@
 describe "ImageInfos API", :type => :request do
 
+  BASE_URL = '/media_gallery/galleries'
+
   before do
     @B64_IMAGE = "R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
-    @user = create(:user)
-    @gallery = create(:gallery, ownable: @user)
+    @gallery1 = create(:gallery)
   end
 
-  it 'gets a list of image infos' do
-    image1 = create(:image_info, gallery: @gallery, label: 'image1')
-    image2 = create(:image_info, gallery: @gallery, label: 'image2')
-    image3 = create(:image_info, gallery: @gallery, label: 'image3')
+  describe 'when showing all' do
 
-    get "/media_gallery/galleries/#{@gallery.id}/image_infos.json", params: {}, headers: { 'token' => @user.token }
+    it 'returns 403 error when anonymous' do
+      delete "#{BASE_URL}/#{@gallery1.id}.json"
+      expect(response).to have_http_status(403)
+      json = JSON.parse(response.body, { symbolize_names: true })
+      expect(json[:message]).to eq('Access Denied')
+    end
 
-    expect(response).to be_success
-    json = JSON.parse(response.body, { symbolize_names: true })
-    json.each { |image| image.delete(:urls) }
+    it 'returns 403 error when resource not present'  do
+      delete "#{BASE_URL}/#{@gallery1.id + 11}.json", headers: { 'token' => @gallery1.ownable.token }
+      expect(response).to have_http_status(403)
+      json = JSON.parse(response.body, { symbolize_names: true })
+      expect(json[:message]).to eq("Access Denied")
+    end
 
-    expect(json).to eq(
-      [
-        { id: image1.id, label: image1.label },
-        { id: image2.id, label: image2.label },
-        { id: image3.id, label: image3.label }
-      ]
-    )
+    it 'return 200 OK when resources are ok' do
+      image1 = create(:image_info, gallery: @gallery1, label: 'b')
+      image2 = create(:image_info, gallery: @gallery1, label: 'c')
+      image3 = create(:image_info, gallery: @gallery1, label: 'a')
+
+      get "#{BASE_URL}/#{@gallery1.id}/image_infos.json", params: {}, headers: { 'token' => @gallery1.ownable.token }
+
+      expect(response).to be_success
+      json = JSON.parse(response.body, { symbolize_names: true })
+      json.each { |image| image.delete(:urls) }
+
+      expect(json).to eq(
+        [
+          { id: image3.id, label: image3.label },
+          { id: image1.id, label: image1.label },
+          { id: image2.id, label: image2.label }
+        ]
+      )
+    end
+
   end
 
-  it 'gets one image info' do
-    image1 = create(:image_info, gallery: @gallery, label: 'image1')
+  describe 'when showing' do
 
-    get "/media_gallery/galleries/#{@gallery.id}/image_infos/#{image1.id}.json", params: {}, headers: { 'token' => @user.token }
+    it '200 OK when resource exists' do
+      image1 = create(:image_info, gallery: @gallery1, label: 'image1')
 
-    expect(response).to be_success
-    json = JSON.parse(response.body, { symbolize_names: true })
-    json.delete(:urls)
-    json.delete(:createdAt)
-    expect(json).to eq(
-      {
-        id: image1.id,
-        label: image1.label,
-        galleryId: @gallery.id,
-        galleryName: @gallery.name,
-        description: 'an image info description'
-      })
+      get "#{BASE_URL}/#{@gallery1.id}/image_infos/#{image1.id}.json", params: {}, headers: { 'token' => @gallery1.ownable.token }
+
+      expect(response).to be_success
+      json = JSON.parse(response.body, { symbolize_names: true })
+      json.delete(:urls)
+      json.delete(:createdAt)
+      expect(json).to eq(
+        {
+          id: image1.id,
+          label: image1.label,
+          galleryId: @gallery1.id,
+          galleryName: @gallery1.name,
+          description: 'an image info description'
+        })
+    end
+
   end
 
-  it 'deletes an existing image info' do
-    image1 = create(:image_info, gallery: @gallery, label: 'image1')
-    delete "/media_gallery/galleries/#{@gallery.id}/image_infos/#{image1.id}.json", params: {}, headers: { 'token' => @user.token }
-    expect(response).to be_success
-    expect(MediaGallery::ImageInfo.exists?(image1.id)).to eq(false)
+  describe 'when updating' do
+
+    it 'returns 200 OK when resource exists' do
+      image1 = create(:image_info, gallery: @gallery1, label: 'image1')
+
+      put "/media_gallery/galleries/#{@gallery1.id}/image_infos/#{image1.id}.json", params: { image_info: { description: "updated description" } }, headers: { 'token' => @gallery1.ownable.token }
+
+      expect(response).to be_success
+      json = JSON.parse(response.body, { symbolize_names: true })
+      json.delete(:createdAt)
+      json.delete(:urls)
+      expect(json).to eq(
+        {
+          id: image1.id,
+          label: image1.label,
+          description: "updated description",
+          galleryId: @gallery1.id,
+          galleryName: @gallery1.name
+        })
+    end
+
   end
 
-  it 'updates an existing gallery' do
-    image1 = create(:image_info, gallery: @gallery, label: 'image1')
+  describe 'when deleting' do
 
-    put "/media_gallery/galleries/#{@gallery.id}/image_infos/#{image1.id}.json", params: { image_info: { description: "updated description" } }, headers: { 'token' => @user.token }
+    it 'returns 403 error when anonymous' do
+      delete "#{BASE_URL}/#{@gallery1.id}.json"
+      expect(response).to have_http_status(403)
+      json = JSON.parse(response.body, { symbolize_names: true })
+      expect(json[:message]).to eq('Access Denied')
+    end
 
-    expect(response).to be_success
-    json = JSON.parse(response.body, { symbolize_names: true })
-    json.delete(:createdAt)
-    json.delete(:urls)
-    expect(json).to eq(
-      {
-        id: image1.id,
-        label: image1.label,
-        description: "updated description",
-        galleryId: @gallery.id,
-        galleryName: @gallery.name
-      })
+    it 'returns 403 error when resource not present' do
+      delete "#{BASE_URL}/#{@gallery1.id + 11}.json", headers: { 'token' => @gallery1.ownable.token }
+      expect(response).to have_http_status(403)
+      json = JSON.parse(response.body, { symbolize_names: true })
+      expect(json[:message]).to eq("Access Denied")
+    end
+
+    it 'returns 200 OK when resource exists' do
+      image1 = create(:image_info, gallery: @gallery1, label: 'image1')
+      delete "/media_gallery/galleries/#{@gallery1.id}/image_infos/#{image1.id}.json", params: {}, headers: { 'token' => @gallery1.ownable.token }
+      expect(response).to be_success
+      expect(MediaGallery::ImageInfo.exists?(image1.id)).to eq(false)
+    end
+
   end
 
-  it 'creates a new image_info' do
-    post "/media_gallery/galleries/#{@gallery.id}/image_infos.json", params: {
-      image_info: {
-        label: "jdoe_image",
-        description: "a description",
-        image: @B64_IMAGE
-      }
-    }, headers: { 'token' => @user.token }
+  describe 'when creating' do
 
-    expect(response).to be_success
-    json = JSON.parse(response.body, { symbolize_names: true })
-    json.delete(:createdAt)
-    json.delete(:urls)
+    it 'returns 200 OK when resource exists' do
+      post "/media_gallery/galleries/#{@gallery1.id}/image_infos.json", params: {
+        image_info: {
+          label: "jdoe_image",
+          description: "a description",
+          image: @B64_IMAGE
+        }
+      }, headers: { 'token' => @gallery1.ownable.token }
 
-    image = MediaGallery::ImageInfo.where(id: json[:id], label: 'jdoe_image', description: 'a description').first
-    expect(json).to eq(
-      {
-        id: image.id,
-        label: image.label,
-        galleryId: image.gallery.id,
-        galleryName: image.gallery.name,
-        description: image.description
-      })
+      expect(response).to be_success
+      json = JSON.parse(response.body, { symbolize_names: true })
+      json.delete(:createdAt)
+      json.delete(:urls)
+
+      image = MediaGallery::ImageInfo.where(id: json[:id], label: 'jdoe_image', description: 'a description').first
+      expect(json).to eq(
+        {
+          id: image.id,
+          label: image.label,
+          galleryId: image.gallery.id,
+          galleryName: image.gallery.name,
+          description: image.description
+        })
+    end
   end
-
 end
